e00f60e190aa3d1575fee375f8be1a3bc684ecde
daiju.watanabe.2plus@gmail.com <11362284+daizyu@users.noreply.github.com>
2022-04-20 21:50:50 +0900

    Add draw.arrow

---
+++ cvplus/draw.py
@@ -0,0 +1,93 @@
+import cv2
+import math
+from enum import IntEnum, auto
+from typing import Any
+
+
+class ArrowShape(IntEnum):
+    none = auto()
+    normal = auto()
+
+
+def arrow(
+    img: cv2.Mat,
+    pt1: Any,
+    pt2: Any,
+    color: Any,
+    thickness: int = 1,
+    line_type: int = cv2.LINE_8,
+    shift: int = 0,
+    arrow_size: float = 10,
+    arrow_angle: float = 30,
+    arrow_shape1: ArrowShape = ArrowShape.normal,
+    arrow_shape2: ArrowShape = ArrowShape.none,
+):
+    """
+    Draw arrow
+
+    Parameters
+    ----------
+    img         : cv2.Mat
+    pt1         : Any
+    pt2         : Any
+    color       : Any
+    thickness   : int = 1,
+    line_type   : int = cv2.LINE_8,
+    shift       : int = 0
+    arrow_size  : float = 10
+    arrow_angle : float = 30
+    arrow_shape1: ArrowShape = ArrowShape.normal
+    arrow_shape2: ArrowShape = ArrowShape.none
+    Returns
+    -------
+    img : cv2.Mat
+    """
+
+    cv2.line(
+        img=img,
+        pt1=pt1,
+        pt2=pt2,
+        color=color,
+        thickness=thickness,
+        lineType=line_type,
+        shift=shift,
+    )
+
+    rad = math.radians(arrow_angle)
+    if arrow_shape1 is ArrowShape.normal:
+        rad1 = math.atan2(pt2[1] - pt1[1], pt2[0] - pt1[0])
+        rad1_1 = rad1 + rad
+        rad1_2 = rad1 - rad
+        x = int(pt1[0] + math.cos(rad1_1) * arrow_size)
+        y = int(pt1[1] + math.sin(rad1_1) * arrow_size)
+        cv2.line(img, pt1, (x, y), color, thickness)
+        x = int(pt1[0] + math.cos(rad1_2) * arrow_size)
+        y = int(pt1[1] + math.sin(rad1_2) * arrow_size)
+        cv2.line(
+            img,
+            pt1,
+            (x, y),
+            color=color,
+            thickness=thickness,
+            lineType=line_type,
+            shift=shift,
+        )
+    if arrow_shape2 is ArrowShape.normal:
+        rad2 = math.atan2(pt1[1] - pt2[1], pt1[0] - pt2[0])
+        rad2_1 = rad2 + rad
+        rad2_2 = rad2 - rad
+        x = int(pt2[0] + math.cos(rad2_1) * arrow_size)
+        y = int(pt2[1] + math.sin(rad2_1) * arrow_size)
+        cv2.line(img, pt2, (x, y), color, thickness)
+        x = int(pt2[0] + math.cos(rad2_2) * arrow_size)
+        y = int(pt2[1] + math.sin(rad2_2) * arrow_size)
+        cv2.line(
+            img,
+            pt2,
+            (x, y),
+            color=color,
+            thickness=thickness,
+            lineType=line_type,
+            shift=shift,
+        )
+    return img
---
+++ doc/lib_draw.md
@@ -0,0 +1,13 @@
+# DRAW module  
+
+## arrow
+Draw arrow    
+``` python
+from cvplus import cvt, draw
+img = cvt.imnew(200,80)
+draw.arrow(img,(20,10),(180,70),(255,255,0),2)
+cvt.imwrite('sample.jpg', img)
+```  
+
+![lib draw arrow](img/lib_draw_001.jpg)
+
