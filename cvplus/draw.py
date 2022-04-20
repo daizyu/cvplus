944992ab8a6f479e258701e7c2f8bd3d6c73da97
daiju.watanabe.2plus@gmail.com <11362284+daizyu@users.noreply.github.com>
2022-04-21 02:18:56 +0900

    Add ArrowShape "side_a" and "side_b"

--- cvplus/draw.py
+++ cvplus/draw.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import cv2
 import math
 from enum import IntEnum, auto
@@ -7,6 +9,8 @@ from typing import Any
 class ArrowShape(IntEnum):
     none = auto()
     normal = auto()
+    side_a = auto()
+    side_b = auto()
 
 
 def arrow(
@@ -54,40 +58,60 @@ def arrow(
     )
 
     rad = math.radians(arrow_angle)
-    if arrow_shape1 is ArrowShape.normal:
+    if arrow_shape1 is not ArrowShape.none:
         rad1 = math.atan2(pt2[1] - pt1[1], pt2[0] - pt1[0])
         rad1_1 = rad1 + rad
         rad1_2 = rad1 - rad
-        x = int(pt1[0] + math.cos(rad1_1) * arrow_size)
-        y = int(pt1[1] + math.sin(rad1_1) * arrow_size)
-        cv2.line(img, pt1, (x, y), color, thickness)
-        x = int(pt1[0] + math.cos(rad1_2) * arrow_size)
-        y = int(pt1[1] + math.sin(rad1_2) * arrow_size)
-        cv2.line(
-            img,
-            pt1,
-            (x, y),
-            color=color,
-            thickness=thickness,
-            lineType=line_type,
-            shift=shift,
-        )
+        if arrow_shape1 is not ArrowShape.side_b:
+            x = int(pt1[0] + math.cos(rad1_1) * arrow_size)
+            y = int(pt1[1] + math.sin(rad1_1) * arrow_size)
+            cv2.line(
+                img,
+                pt2,
+                (x, y),
+                color=color,
+                thickness=thickness,
+                lineType=line_type,
+                shift=shift,
+            )
+        if arrow_shape1 is not ArrowShape.side_a:
+            x = int(pt1[0] + math.cos(rad1_2) * arrow_size)
+            y = int(pt1[1] + math.sin(rad1_2) * arrow_size)
+            cv2.line(
+                img,
+                pt1,
+                (x, y),
+                color=color,
+                thickness=thickness,
+                lineType=line_type,
+                shift=shift,
+            )
     if arrow_shape2 is ArrowShape.normal:
         rad2 = math.atan2(pt1[1] - pt2[1], pt1[0] - pt2[0])
         rad2_1 = rad2 + rad
         rad2_2 = rad2 - rad
-        x = int(pt2[0] + math.cos(rad2_1) * arrow_size)
-        y = int(pt2[1] + math.sin(rad2_1) * arrow_size)
-        cv2.line(img, pt2, (x, y), color, thickness)
-        x = int(pt2[0] + math.cos(rad2_2) * arrow_size)
-        y = int(pt2[1] + math.sin(rad2_2) * arrow_size)
-        cv2.line(
-            img,
-            pt2,
-            (x, y),
-            color=color,
-            thickness=thickness,
-            lineType=line_type,
-            shift=shift,
-        )
+        if arrow_shape2 is not ArrowShape.side_a:
+            x = int(pt2[0] + math.cos(rad2_1) * arrow_size)
+            y = int(pt2[1] + math.sin(rad2_1) * arrow_size)
+            cv2.line(
+                img,
+                pt2,
+                (x, y),
+                color=color,
+                thickness=thickness,
+                lineType=line_type,
+                shift=shift,
+            )
+        if arrow_shape2 is not ArrowShape.side_b:
+            x = int(pt2[0] + math.cos(rad2_2) * arrow_size)
+            y = int(pt2[1] + math.sin(rad2_2) * arrow_size)
+            cv2.line(
+                img,
+                pt2,
+                (x, y),
+                color=color,
+                thickness=thickness,
+                lineType=line_type,
+                shift=shift,
+            )
     return img
